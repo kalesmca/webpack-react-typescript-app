7433cad3f310d6dc5b307dc54a1d040f17dccb56
kaleeswaran <kalesmca@gmail.com>
2022-01-16 13:20:17 +0530

    styles and images adde

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import scb from './assets/images/scb.png';
 
 const App = () => {
     return(
-        <div>React Application basic setup not yet done deployments</div>
+        <div>React Application basic setup not yet done deployments
+            <img src={scb} height={400} width={400}/>
+        </div>
     )
 }
 
