f28c9cef27a875f8299c9ad36d7671082df79df5
kaleeswaran <kalesmca@gmail.com>
2022-01-16 14:02:49 +0530

    evironment set has been done

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,8 @@ import scb from './assets/images/scb.png';
 
 const App = () => {
     return(
-        <div>React Application basic setup not yet done deployments
+        <div>React Application basic setup not yet done deployments {process.env.NODE_ENV}
+        name is : {process.env.name}
             <img src={scb} height={400} width={400}/>
         </div>
     )
